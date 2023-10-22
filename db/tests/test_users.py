01b986196a313d4f97681c2202b273f969c26160
Jia lin Weng <jialinweng@Jias-MacBook-Air.local>
2023-10-21 21:13:06 -0400

    Merge changes in test_users.py

---
+++ db/tests/test_users.py
@@ -0,0 +1,12 @@
+import db.users as usrs
+
+
+def test_get_users():
+    users = usrs.get_users()
+    assert isinstance(users, dict)
+    assert len(users) > 0  # at least one user!
+    for key in users:
+        assert isinstance(key, str)
+        assert len(key) >= usrs.MIN_USER_NAME_LEN
+        user = users[key]
+        assert isinstance(user, dict)
