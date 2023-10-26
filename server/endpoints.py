30bf4f4b3a5b057d7c934a2ff4d06ede1d98e2d7
afanc0 <gianromani.gr@gmail.com>
2023-10-26 12:48:50 -0400

    restructured the endpoints.py file

--- server/endpoints.py
+++ server/endpoints.py
@@ -6,7 +6,6 @@ The endpoint called `endpoints` will return all available endpoints.
 from flask import Flask
 from flask import request
 from flask_restx import Resource, Api
-# import db.db as db
 
 import db.users as users
 
@@ -16,12 +15,13 @@ api = Api(app)
 HELLO_EP = '/hello'
 HELLO_RESP = 'hello'
 
+# MENUS
 MAIN_MENU = '/MainMenu'
 MAIN_MENU_NM = "Welcome to Food Finder!"
-
 LOGIN_PAGE = '/LoginPage'
 LOGIN_SCREEN_MSG = 'Please Login or Register'
 
+# ENDPOINTS
 LOGIN_SYSTEM = '/LoginSystem'
 REGISTRATION_SYSTEM = '/RegistrationSystem'
 
@@ -33,9 +33,6 @@ MENU = 'menu'
 
 USER_MENU_EP = '/user_menu'
 MAIN_MENU_EP = '/MainMenu'
-
-CLIENT_MENU_EP = '/ClientMenu'
-RESTAURANT_MENU_EP = '/RestaurantMenu'
 
 
 @api.route('/hello')
@@ -66,7 +63,24 @@ class Endpoints(Resource):
         return {"Available endpoints": endpoints}
 
 
-# START OF PROJECT #
+@api.route(f'{MAIN_MENU}')
+class MainMenu(Resource):
+    """
+    This will deliver our main menu.
+    """
+    def get(self):
+        """
+        Gets the main app menu.
+        """
+        return {'Title': MAIN_MENU_NM,
+                'Default': 1,
+                'Choices': {
+                    '1': {'url': '/', 'method': 'get',
+                          'text': 'Find Food!'},
+                    '2': {'url': '/',
+                          'method': 'get', 'text': 'Open a Store!'},
+                    'X': {'text': 'Exit'},
+                }}
 
 
 @api.route(f'{LOGIN_PAGE}')
@@ -88,6 +102,9 @@ class LoginPage(Resource):
                           'method': 'get', 'text': 'Sign-Up'},
                     'X': {'text': 'Exit'},
                 }}
+
+
+# START OF PROJECT #
 
 
 @api.route(f'{LOGIN_SYSTEM}')
@@ -145,19 +162,7 @@ class RegistrationSystem(Resource):
     def post(self):
         """
         Takes care of login information with the entered data information
-
-        :param email: The email of the user
-        :param password: The password of the user
-
-        :return: Registration Complete Message
         """
-
-        # The information will be retreieved from the data
-        # Email and Password will be the two given parameters
-
-        # check username in database and if it then return True or else.
-        # later on this will be changed when we have our database.
-        # checks if the data information is correct
 
         data = request.get_json()
         user_email = data.get("user_email")
@@ -188,6 +193,7 @@ class RegistrationSystem(Resource):
                         "SYSTEM_STATUS": "FAILED"
                     }, 200
 
+            # Create a new record in database once that's up
             return {
                 "SYSTEM_STATUS": "PASSED"
             }, 200
@@ -201,67 +207,3 @@ class RegistrationSystem(Resource):
                 "SYSTEM_STATUS": "FAILED",
                 "ERROR_MESSAGE": str(error)
             }, 406
-
-
-@api.route(f'{MAIN_MENU}')
-class MainMenu(Resource):
-    """
-    This will deliver our main menu.
-    """
-    def get(self):
-        """
-        Gets the main app menu.
-        """
-        return {'Title': MAIN_MENU_NM,
-                'Default': 1,
-                'Choices': {
-                    '1': {'url': '/', 'method': 'get',
-                          'text': 'Find Food!'},
-                    '2': {'url': '/',
-                          'method': 'get', 'text': 'Open a Store!'},
-                    'X': {'text': 'Exit'},
-                }}
-
-
-# @api.route(f'{CLIENT_MENU_EP}')
-# class ClientMenu(Resource):
-#     """
-#     Displays Client Main Menu
-#     """
-#     def get(self):
-#         """
-#         This method will deliver the client main menu
-#         """
-#         return {
-#         }
-
-
-# @api.route(f'{RESTAURANT_MENU_EP}')
-# class RestaurantMenu(Resource):
-#     """
-#     Displays Restaurant Main Menu
-#     """
-#     def get(self):
-#         """
-#         This method will deliver the Restaurant main menu
-#         """
-#         return {
-#         }
-
-
-# @api.route(f'{USERS}')
-# class Users(Resource):
-#     """
-#     This class supports fetching a list of all users.
-#     """
-#     def get(self):
-#         """
-#         This method returns all users.
-#         """
-#         return {
-#             TYPE: DATA,
-#             TITLE: 'Current USER',
-#             DATA: users.get_users(),
-#             MENU: USER_MENU_EP,
-#             RETURN: MAIN_MENU_EP,
-#         }
