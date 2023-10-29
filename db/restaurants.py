490f02ced93e76744a3c3cb794409ff3cd935d47
afanc0 <gianromani.gr@gmail.com>
2023-10-29 14:11:07 -0400

    restaurant db

--- db/restaurants.py
+++ db/restaurants.py
@@ -2,27 +2,37 @@
 restaurants.py: the interface to our restaurant data.
 """
 
-ADDRESS = 'restaurantAddress'
-TEST_RESTAURANT_NAME = 'Food Store'
+ADDRESS = 'address'
+ZIPCODE = 'zipcode'
+NAME = 'name'
+OWNER_ID = 'owner_id'
+RESTAURANT_ID = 'restaurant_id'
 
 resturants = {
-    'Terrific Tacos': {
-        ADDRESS: '92 Water Ave',
+    "User_1": {
+        NAME: "Taco Spot",
+        ADDRESS: "92 Water Ave",
+        ZIPCODE: "10004",
+        OWNER_ID: 1,
+        RESTAURANT_ID: 9
     },
-    TEST_RESTAURANT_NAME: {
-        ADDRESS: '242 Chicken Street',
+    "User_2": {
+        NAME: "Italian Spot",
+        ADDRESS: "242 Chicken Street",
+        ZIPCODE: "10004",
+        OWNER_ID: 23,
+        RESTAURANT_ID: 12
     },
 }
 
 
-def get_restaurants() -> dict:
-    return resturants
+def get_nearby_restaurants(zip_code: str):
+    nearby_rest = {}
+    for rest_key in resturants:
+        if (resturants[rest_key][ZIPCODE] == zip_code):
+            nearby_rest = resturants[rest_key]
+    return nearby_rest
 
 
-def add_restaurant(store_name: str,  store_address: str):
-    if store_name in resturants:
-        raise ValueError(f'Error duplicate restaurant name: {store_name=}')
-    if len(store_name) == 0:
-        raise ValueError('Fill out the restaurant name')
-
-    resturants[store_name] = {ADDRESS: store_address}
+def add_restaurant(store_name: str,  store_address: str, store_zipcode: str, store_owner_id: int):
+    pass
