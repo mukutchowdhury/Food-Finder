cac6a05e0e52190af2b8df2cd40e56d16832b27e
Masrur Kabir <freshcake189@gmail.com>
2023-10-29 23:56:51 -0400

    test ratings

--- db/ratings.py
+++ db/ratings.py
@@ -6,7 +6,7 @@ RATING = 'restaurantRating'
 POSSIBLE_RATINGS = ['Terrible', 'Poor', 'Ok', 'Good', 'Excellent']
 TEST_RESTAURANT_NAME = 'Food Store'
 
-resturant_ratings = {
+ratings = {
     'Terrific Tacos': {
         RATING: 'Terrible',
     },
@@ -16,16 +16,16 @@ resturant_ratings = {
 }
 
 
-def get_restaurants() -> dict:
-    return resturant_ratings
+def get_ratings() -> dict:
+    return ratings
 
 
 def add_restaurant_rating(store_name: str,  store_rating: str):
-    if store_name in resturant_ratings:
+    if store_name in ratings:
         raise ValueError(f'Error duplicate restaurant name: {store_name=}')
     if len(store_name) == 0:
         raise ValueError('Fill out the restaurant name')
     if store_rating not in POSSIBLE_RATINGS:
         raise ValueError('Error invalid restaurant rating')
 
-    resturant_ratings[store_name] = {RATING: store_rating}
+    ratings[store_name] = {RATING: store_rating}
