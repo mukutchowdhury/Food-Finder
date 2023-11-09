a7d9155f0cb3e17910a7e6315cfbf3c7f3135c0e
afanc0 <gianromani.gr@gmail.com>
2023-11-08 21:29:49 -0500

    patch good and bad test for rest_reg endpoint

--- server/tests/test_endpoints.py
+++ server/tests/test_endpoints.py
@@ -136,52 +136,16 @@ def test_addmenuitem():
 
 
 ### Restaurant Registration Tests ###
+@patch('db.restaurants.add_restaurant', side_effect=None, autospec=True)
+def test_good_restaurant_registration(mock_add):
+    resp = TEST_CLIENT.post(ep.RESTAURANT_REGISTRATION, json=rest.get_test_restaurant())
+    assert resp.status_code == OK
+
+
 @patch('db.restaurants.add_restaurant', side_effect=ValueError, autospec=True)
-def test_existing_restaurant_registration(mock_add):
+def test_bad_restaurant_registration(mock_add):
     resp = TEST_CLIENT.post(ep.RESTAURANT_REGISTRATION, json=rest.get_test_restaurant())
     assert resp.status_code == NOT_ACCEPTABLE
-
-
-def test_restaurant_registration():
-    user_json = {
-        "rest_name": "Imperial Fish", 
-        "rest_address": "439 Somewhere St", 
-        "rest_zipcode": "10002"
-    }
-    resp = TEST_CLIENT.post(ep.RESTAURANT_REGISTRATION, json=user_json)
-    assert resp.status_code == 200
-    resp_json = resp.get_json()
-    assert "SYSTEM_STATUS" in resp_json
-    print(f'Restaurant Registrated: {resp_json["SYSTEM_STATUS"]}')
-    assert "PASSED" in resp_json["SYSTEM_STATUS"]
-
-
-def test_existing_restaurant_registration():
-    user_json = {
-        "rest_name": "Imperial Fish", 
-        "rest_address": "242 Chicken Street", 
-        "rest_zipcode": "10002"
-    }
-    resp = TEST_CLIENT.post(ep.RESTAURANT_REGISTRATION, json=user_json)
-    assert resp.status_code == 406
-    resp_json = resp.get_json()
-    assert "SYSTEM_STATUS" in resp_json
-    print(f'Restaurant Registrated: {resp_json["SYSTEM_STATUS"]}')
-    assert "FAILED" in resp_json["SYSTEM_STATUS"]
-
-
-def test_empty_restaurant_registration():
-    user_json = {
-        "rest_name": "", 
-        "rest_address": "", 
-        "rest_zipcode": ""
-    }
-    resp = TEST_CLIENT.post(ep.RESTAURANT_REGISTRATION, json=user_json)
-    assert resp.status_code == 406
-    resp_json = resp.get_json()
-    assert "SYSTEM_STATUS" in resp_json
-    print(f'Restaurant Registrated: {resp_json["SYSTEM_STATUS"]}')
-    assert "FAILED" in resp_json["SYSTEM_STATUS"]
 
 
     # Try to add an item with the same name as an existing item, giving a fail message
