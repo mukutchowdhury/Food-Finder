7ff88c980918df0b78a66f062f7bdde43e7b1239
afanc0 <gianromani.gr@gmail.com>
2023-11-25 23:09:18 -0500

    setup db connection for reservation

--- db/reservations.py
+++ db/reservations.py
@@ -3,9 +3,15 @@ reservations.py: contains reservations made by users
 and stored using restaurant_id as key
 """
 
+import db.db_connect as dbc
+
 USERNAME = 'username'
 TIME = 'time'
 PARTY_SIZE = 'party_size'
+
+RESERVATION_DB = 'reservationdb'
+RESTAURANT_ID = 'restaurant_id'
+RESERV_COLLECT = 'reservations'
 
 # reservations can be made by users
 reservations = {
@@ -18,14 +24,16 @@ reservations = {
 
 
 def get_all_reservations():
-    return reservations
+    dbc.connect_db()
+    return dbc.fetch_all_as_dict(RESTAURANT_ID, RESERV_COLLECT, RESERVATION_DB)
 
 
-def get_rest_reservation(restaurant):
-    if restaurant not in reservations:
-        raise ValueError("no reservations made for " + restaurant)
+def get_rest_reservation(restaurant_id):
+    if restaurant_id not in reservations:
+        raise ValueError("no reservations made for " + restaurant_id)
 
-    return reservations[restaurant]
+    return dbc.fetch_one(RESERV_COLLECT, {RESTAURANT_ID: restaurant_id}, RESERVATION_DB)
+    # return reservations[restaurant]
 
 
 def make_reservation(restaurant, user_name, time, party_size):
@@ -37,3 +45,7 @@ def make_reservation(restaurant, user_name, time, party_size):
         TIME: time,
         PARTY_SIZE: party_size
     })
+
+    dbc.connect_db()
+    _id = dbc.insert_one(RESERV_COLLECT, restaurant, RESERVATION_DB)
+    return _id is not None
