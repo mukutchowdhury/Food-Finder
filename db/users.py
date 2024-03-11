3993b09410c3c2cfe96a8e6347c3f3a4fadfd8df
afanc0 <gianromani.gr@gmail.com>
2024-03-11 12:36:57 -0400

    added user login and signup logic

--- db/users.py
+++ db/users.py
@@ -1,48 +1,48 @@
-"""
-This module interfaces to our user data.
-"""
+import db.db_connect as dbc
+import bcrypt
+import random
 
-MIN_USER_NAME_LEN = 1
-MIN_EMAIL_LEN = 1
-EMAIL = 'EMAIL'
-PASSWORD = 'PASSWORD'
+# 1E => Account Exists
 
-# Eric's password -> ericiscool
-# John's password -> JOHN123
+BIG_NUM = 1_000_000_000
 
-users = {
-        "User_1": {
-            EMAIL: 'app123@gmail.com',
-            PASSWORD: (
-                b'$2b$12$0AZZGPgP7MCwyGwn7KR58eRWyvsJw8WrnjCqy6n.'
-                b'gE9OF8V/ayQ/G'
-            )
-        },
-        "User_2": {
-            EMAIL: 'ora123@gmail.com',
-            PASSWORD: (
-                b'$2b$12$.s3eFLLJqiPzHSUM.0VVAuW3Alt4TJH'
-                b'VcxLdLG.k.jnEFGR3X0WwW'
-            )
-        },
-    }
+USER_ID = 'user_id'
+EMAIL = 'email'
+PASSWORD = 'password'
+USERS_COLLECT = 'users'
+FNAME = 'fname'
+LNAME = 'lname'
 
 
-# Updates 'users' with a new user entry
-def add_user(user_email: str, user_password: bytes):
-    user_key = f'User_{len(users)}'
-    users[user_key] = {
-        EMAIL: user_email,
-        PASSWORD: user_password
-    }
+def exists(email: str) -> bool:
+    dbc.connect_db()
+    return dbc.fetch_one(USERS_COLLECT, {EMAIL: email})
 
 
-def get_users():
-    """
-    Our contract:
-        - No arguments.
-        - Returns a dictionary of users keyed on user name (a str).
-        - Each user name must be the key for a dictionary.
-        -The dictionary should include at least one email in the form of a str.
-    """
-    return users
+def _gen_user_id():
+    user_id = random.randint(0, BIG_NUM)
+    return user_id
+
+
+def add_user(email: str, password: str, fname: str, lname: str):
+    if (exists(email)):
+        raise ValueError('1B')
+    byte_password = password.encode('utf-8')
+    hashed_password = bcrypt.hashpw(byte_password, bcrypt.gensalt())
+    user_id = _gen_user_id()
+    USER = {USER_ID: user_id, EMAIL: email, PASSWORD: hashed_password,
+            FNAME: fname, LNAME: lname}
+    dbc.connect_db()
+    _id = dbc.insert_one(USERS_COLLECT, USER)
+    return _id is not None
+
+
+def get_user(email: str, password: str):
+    if (exists(email)):
+        result = dbc.fetch_one(USERS_COLLECT, {EMAIL: email})
+        byte_password = password.encode('utf-8')
+        if not bcrypt.checkpw(byte_password, result[PASSWORD]):
+            raise ValueError('1C')
+        return result[USER_ID]
+    else:
+        raise ValueError('1A')
