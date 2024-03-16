659061d3740471c0172d683a6ee7a9b8c4f7dbef
afanc0 <gianromani.gr@gmail.com>
2024-03-15 21:02:55 -0400

    created category db with get and add functions

---
+++ db/categories.py
@@ -0,0 +1,24 @@
+import db.db_connect as dbc
+
+CATEGORY_COLLECTION = 'categories'
+NAME = 'name'
+DESCRIPTION = 'description'
+
+
+def addCategory(name: str, descpription: str = ""):
+    if (exists(name)):
+        raise ValueError(f'Category name exists, {name}')
+    CATEGORY = {NAME: name, DESCRIPTION: descpription}
+    dbc.connect_db()
+    _id = dbc.insert_one(CATEGORY_COLLECTION, CATEGORY)
+    return _id is not None
+
+
+def getCategories():
+    dbc.connect_db()
+    return dbc.fetch_all_as_dict(NAME, CATEGORY_COLLECTION)
+
+
+def exists(name: str) -> bool:
+    dbc.connect_db()
+    return dbc.fetch_one(CATEGORY_COLLECTION, {NAME: name})
