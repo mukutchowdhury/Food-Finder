3a61fd2657b1212cf76b931a0f953a88ae9204d1
afanc0 <gianromani.gr@gmail.com>
2024-03-15 22:07:56 -0400

    updated user shcema

--- db/users.py
+++ db/users.py
@@ -12,6 +12,7 @@ PASSWORD = 'password'
 USERS_COLLECT = 'users'
 FNAME = 'fname'
 LNAME = 'lname'
+PIMAGE = 'pimage'
 
 
 def exists(email: str) -> bool:
@@ -24,14 +25,14 @@ def _gen_user_id():
     return user_id
 
 
-def add_user(email: str, password: str, fname: str, lname: str):
+def add_user(email: str, password: str, fname: str, lname: str, pimage: str = ""):
     if (exists(email)):
         raise ValueError('1B')
     byte_password = password.encode('utf-8')
     hashed_password = bcrypt.hashpw(byte_password, bcrypt.gensalt())
     user_id = _gen_user_id()
     USER = {USER_ID: user_id, EMAIL: email, PASSWORD: hashed_password,
-            FNAME: fname, LNAME: lname}
+            FNAME: fname, LNAME: lname, PIMAGE: pimage}
     dbc.connect_db()
     _id = dbc.insert_one(USERS_COLLECT, USER)
     return _id is not None
