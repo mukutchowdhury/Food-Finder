31187e84456e967a6f0c0dd89d117af08a838bdf
afanc0 <gianromani.gr@gmail.com>
2024-03-29 01:55:23 -0400

    created test cases for categories at 100 coverage

---
+++ db/tests/test_categories.py
@@ -0,0 +1,46 @@
+import pytest
+
+import db.categories as cat
+
+TEST_NAME = 'test_name'
+TEST_DESCRIPTION = 'test_description'
+
+
+@pytest.fixture(scope='function')
+def temp_category():
+    ret = cat.addCategory(TEST_NAME, TEST_DESCRIPTION)
+    yield TEST_NAME
+    if cat.exists(TEST_NAME):
+        cat.deleteCategory(TEST_NAME)
+
+
+def test_addCategory():
+    ret = cat.addCategory(TEST_NAME, TEST_DESCRIPTION)
+    assert cat.exists(TEST_NAME)
+    assert isinstance(ret, bool)
+    cat.deleteCategory(TEST_NAME)
+
+
+def test_addCategory_duplicate(temp_category):
+    with pytest.raises(ValueError):
+        cat.addCategory(temp_category, TEST_DESCRIPTION)
+
+
+def test_getCategories(temp_category):
+    categories = cat.getCategories()
+    assert isinstance(categories, dict)
+    for category in categories:
+        assert isinstance(category, str)
+        assert isinstance(categories[category], dict)
+    assert cat.exists(temp_category)
+
+
+def test_deleteCategory(temp_category):
+    categoryName = temp_category
+    cat.deleteCategory(categoryName)
+    assert not cat.exists(categoryName)
+
+
+def test_deleteCategory_NotFound():
+    with pytest.raises(ValueError):
+        cat.deleteCategory(TEST_NAME)
