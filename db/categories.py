d5513c5f1424fb3463249b9b09527f18b85c15ea
afanc0 <gianromani.gr@gmail.com>
2024-04-19 23:01:32 -0400

    created test category function

--- db/categories.py
+++ db/categories.py
@@ -1,8 +1,27 @@
 import db.db_connect as dbc
+import random
 
 CATEGORY_COLLECTION = 'categories'
 NAME = 'name'
 DESCRIPTION = 'description'
+
+ID_LEN = 24
+BIG_NUM = 100_000_000_000_000_000_000
+
+MOCK_ID = '0' * ID_LEN
+
+
+def _get_test_name():
+    name = 'test'
+    rand_part = random.randint(0, BIG_NUM)
+    return name + str(rand_part)
+
+
+def get_test_category():
+    test_category = {}
+    test_category[NAME] = _get_test_name()
+    test_category[DESCRIPTION] = ''
+    return test_category
 
 
 def addCategory(name: str, descpription: str = ""):
