74b042c3f7439ee2d20adb55c3fad738e33676da
afanc0 <gianromani.gr@gmail.com>
2024-04-20 16:04:08 -0400

    added fucntions to gen test objects

--- db/ratings.py
+++ db/ratings.py
@@ -16,6 +16,8 @@ RATING_COLLECT = 'ratings'
 RATING_DB = 'ratingsdb'
 REST_COLLECT = 'restaurants'
 
+STATUS = 'status'
+
 
 def _gen_reviewId() -> str:
     _id = random.randint(0, BIG_NUM)
@@ -24,24 +26,50 @@ def _gen_reviewId() -> str:
     return _id
 
 
+def get_test_rating():
+    test_rating = {}
+    test_rating[USER_ID] = _gen_reviewId()
+    test_rating[TEXT] = 'TEST'
+    test_rating[STAR] = 5
+    return test_rating
+
+
+def get_test_add_return():
+    test_return = {}
+    test_return[STATUS] = MOCK_ID
+    test_return[REVIEW_ID] = _gen_reviewId()
+    return test_return
+
+
+def get_test_bad_add_return():
+    test_return = {}
+    test_return[STATUS] = None
+    test_return[REVIEW_ID] = _gen_reviewId()
+    return test_return
+
+
+def get_test_update_rating():
+    return {TEXT: 'TEST'}
+
+
 def review_exists(review_id: str) -> bool:
     dbc.connect_db()
     return dbc.fetch_one(RATING_COLLECT, {REVIEW_ID: review_id})
 
 
-def restaurant_exists(restaurant_id: int) -> bool:
+def restaurant_exists(restaurant_id: str) -> bool:
     dbc.connect_db()
     return dbc.fetch_one(REST_COLLECT, {RESTAURANT_ID: restaurant_id})
 
 
-def get_restaurant_ratings(restaurant_id: int):
+def get_restaurant_ratings(restaurant_id: str):
     if restaurant_exists(restaurant_id):
         return dbc.fetch_all_by_key(RATING_COLLECT,
                                     {RESTAURANT_ID: restaurant_id})
     raise ValueError(f'Get failure: {restaurant_id} not found.')
 
 
-def add_restaurant_rating(restaurant_id: int, user_id: int,
+def add_restaurant_rating(restaurant_id: str, user_id: str,
                           text: str, star: int):
     if not restaurant_exists(restaurant_id):
         raise ValueError(f'Post failure: {restaurant_id} not found.')
@@ -64,7 +92,7 @@ def add_restaurant_rating(restaurant_id: int, user_id: int,
     }
 
 
-def update_review_text(review_id: int, text: str):
+def update_review_text(review_id: str, text: str):
     if review_exists(review_id):
         return dbc.up_one(
             RATING_COLLECT,
@@ -74,7 +102,7 @@ def update_review_text(review_id: int, text: str):
     raise ValueError(f'Update failure: Review: {review_id} not in database.')
 
 
-def del_rating(review_id: int):
+def del_rating(review_id: str):
     if review_exists(review_id):
         return dbc.del_one(
             RATING_COLLECT,
